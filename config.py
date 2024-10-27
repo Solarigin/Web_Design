cfcb55acd16c45d890d2462cf194510cf71a8f1a
Larigin_Cheng <68094323+Solarigin@users.noreply.github.com>
2024-10-27 15:36:14 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,18 +1,10 @@
 # config.py
 db_config = {
     'user': 'root',
-    'password': '8935332',
+    'password': '12345678',
     'host': 'localhost',
-    'database': 'nsm',
+    'database': 'DB_name',
 }
-
-# 删除或注释掉 Redis 配置
-# redis_config = {
-#     'host': 'localhost',
-#     'port': 6379,
-#     'db': 0,
-# }
-
 
 # Adding secret_key to config
 secret_key = 'your-very-secret-key'
